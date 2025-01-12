0a18748fedc3501d75d35fb0b58bf31e57e1d995
shimabox <shimabox@gmail.com>
2025-01-12 14:25:23 +0900

    feat: coverageをとる
    npm install --save-dev @vitest/coverage-v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './coverage',
+      all: true, // テストされていないコードもカバレッジに含める
+      include: ['src/**/*.{ts,js}'],
+      exclude: ['node_modules', 'dist'],
+    },
   },
 });
