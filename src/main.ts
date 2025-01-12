89f67bc2ea90a3a64a0d2abd0b5a1d31402d928a
shimabox <shimabox@gmail.com>
2025-01-12 19:40:10 +0900

    feat: Hをたくさん描画
    水生成シミュレーターを使うようにしてHをたくさん描画

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
-import { Coordinate } from './atoms/Coordinate';
-import { H } from './atoms/H';
+import { MizuSimulator } from './simulator/MizuSimulator';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.querySelector('#myCanvas') as HTMLCanvasElement;
-  if (!canvas) {
-    throw new Error('Canvas not found.');
-  }
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  document.body.appendChild(canvas);
-
-  const ctx = canvas.getContext('2d');
-  if (!ctx) {
-    throw new Error('Canvas context is not available.');
-  }
-
-  const hAtom = new H(canvas.width);
-  hAtom.initializeDrawingProperties(
-    new Coordinate(canvas.width * Math.random(), canvas.height * Math.random()),
-  );
-  hAtom.render(ctx);
+  const simulator = new MizuSimulator();
+  const scale = simulator.getScale();
+  simulator.init(30 * scale);
+  simulator.renderFrame();
 });
