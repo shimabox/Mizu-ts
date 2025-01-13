ac4b17576b22be0849cd8b671609999cc76ec891
shimabox <shimabox@gmail.com>
2025-01-13 23:48:42 +0900

    chore: GitHub Pages用の設定

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? 'Mizu-ts' : './',
   plugins: [
     checker({
       typescript: true,
